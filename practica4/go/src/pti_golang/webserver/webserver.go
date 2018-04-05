da19367eae0a63b44b9e9caeb2ab83d00e235d47
Alba <alba.canete.garrucho@est.fib.upc.edu>
2018-04-05 11:01:02 +0200

    new rental v1

--- practica4/go/src/pti_golang/webserver/webserver.go
+++ practica4/go/src/pti_golang/webserver/webserver.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 ) 
 
-
 type ResponseMessage struct {
     Field1 string
     Field2 string
@@ -21,15 +20,23 @@ type RequestMessage struct {
     Field2 string
 }
 
+type carRentalRequest struct {
+	CarMaker string
+	CarModel string
+	NumberDays int
+	NumberUnits int
+}
+
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/endpoint/{param}", endpointFunc)
+	router.HandleFunc("/endpoint2/{param}", endpointFunc2JSONInput)
+	router.HandleFunc("/newrental/{param}", newrentalFunc)
+	//router.HandleFunc("/listrentals/{param}", listrentalsFunc)
 
-router := mux.NewRouter().StrictSlash(true)
-router.HandleFunc("/", Index)
-router.HandleFunc("/endpoint/{param}", endpointFunc)
-router.HandleFunc("/endpoint2/{param}", endpointFunc2JSONInput)
-
-log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -61,5 +68,30 @@ func endpointFunc2JSONInput(w http.ResponseWriter, r *http.Request) {
     } else {
         fmt.Fprintln(w, "Successfully received request with Field1 =", requestMessage.Field1)
         fmt.Println(r.FormValue("queryparam1"))
+    }
+}
+
+func newrentalFunc (w http.ResponseWriter, r *http.Request) {
+	var requestMessage carRentalRequest
+    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if err != nil {
+        panic(err)
+    }
+    if err := r.Body.Close(); err != nil {
+        panic(err)
+    }
+    if err := json.Unmarshal(body, &requestMessage); err != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(422) // unprocessable entity
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    } else {
+        fmt.Fprintln(w, "Successfully received request for new car rental with name ", r.FormValue("name"))
+        fmt.Println("Car maker: ", requestMessage.CarMaker)
+        fmt.Println("Car model: ", requestMessage.CarModel)
+        fmt.Println("Number of days: ", requestMessage.NumberDays)
+        fmt.Println("Number of units: ", requestMessage.NumberUnits)
+        fmt.Println("Preu: ", 1000)
     }
 }
